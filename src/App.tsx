28d1fd8f76ded0d93c170ab8d02ddeafb5bc00a6
Luis Brodely Tovar Escobar <ltovar@neodata.com.mx>
2023-10-02 02:19:00 -0600

    arreglando bug

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,8 @@
 import React, { useState } from 'react';
-
-const isValidCharacter = (char: string, position: number, previousValid: boolean): boolean => {
-  if (position === 3 && previousValid) {
-    return /^[0-9]$/.test(char);
-  }
-
-  if (position === 0 || position === 1 || position === 2) {
-    if (previousValid) {
-      return /^[0-9]$/.test(char);
-    } else {
-      return /^[1-9]$/.test(char);
-    }
-  }
-
-  return false;
-};
-
-const isInRange = (value: string): boolean => {
-  const intValue = parseInt(value, 10);
-  return intValue >= 1 && intValue <= 9999;
-};
-
 const App: React.FC = () => {
   const [inputValue, setInputValue] = useState<string>('');
   const [characters, setCharacters] = useState<string[]>([]);
+  const [numberValid, setNumberValid] = useState<boolean>(false)
   const [invalidCharacterDetected, setInvalidCharacterDetected] = useState<boolean>(false);
 
   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -48,16 +27,76 @@ const App: React.FC = () => {
       }
     }
 
-    if (validChars.length === 4) {
-      const value = validChars.join('');
-      if (!isInRange(value)) {
-        invalidCharacterFound = true;
-      }
-    }
-
     setInputValue(input);
     setCharacters(validChars);
     setInvalidCharacterDetected(invalidCharacterFound);
+  };
+
+  const isValidCharacter = (char: string, position: number, previousValid: boolean): boolean => {
+
+    if (position === 0 && previousValid) {
+      return /^[vw]$/.test(char);
+    }
+  
+    if (position === 1 && previousValid) {
+      if (inputValue[0] === 'v') {
+        return /^[u-z]$/.test(char);
+      } else if (inputValue[0] === 'w') {
+        return /^[a-x]$/.test(char);
+      }
+    }
+  
+    if (position === 2 && previousValid) {
+      return /^[-]$/.test(char);
+    }
+  
+    if (position === 3 && previousValid) {
+      if (char === '0') {
+        setNumberValid(false)
+        return /^[0]$/.test(char);
+      } else {
+        setNumberValid(true)
+        return /^[1-9]$/.test(char);
+      }     
+    }
+
+    if (position === 4 && previousValid) {
+      if (char === '0') {
+        setNumberValid(false)
+        return /^[0]$/.test(char);
+      } else {
+        setNumberValid(true)
+        return /^[1-9]$/.test(char);
+      }  
+    }
+
+    if (position === 5 && previousValid) {
+      if (char === '0') {
+        setNumberValid(false)
+        return /^[0]$/.test(char);
+      } else {
+        setNumberValid(true)
+        return /^[1-9]$/.test(char);
+      }    
+    }
+  
+    if (position === 6 && previousValid) {
+      if (numberValid) {
+        return /^[0-9]$/.test(char);
+      } else {
+        return /^[1-9]$/.test(char);
+      }
+    }
+
+    if (position === 7 && previousValid) {
+      return /^[-]$/.test(char);
+    }
+
+    if (position === 8 && previousValid) {
+      return /^[a-z]$/.test(char);
+    }
+  
+    return false;
   };
 
   return (
